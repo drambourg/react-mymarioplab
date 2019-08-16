b4d39f5e3bc180b70842c8388eaf945178fd9ab9
xNousex <damien.rambourg@gmail.com>
2019-08-16 15:27:02 +0200

    Mise en place de materialize Style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Mario Plan</h1>
     </div>
   );
 }
