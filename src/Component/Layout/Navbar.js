ee38d7b4c2e2532753b2e7d8550e61a1c83d4a8b
xNousex <damien.rambourg@gmail.com>
2019-08-16 15:54:14 +0200

    Implement Link Signin

--- src/Component/Layout/Navbar.js
+++ src/Component/Layout/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import SignedInLinks from "./SignedInLinks";
 
 const Navbar = () => {
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
-                <NavLink to='/' className="brand-logo">MarioPlan</NavLink>
+                <Link to='/' className="brand-logo left">MarioPlan</Link>
+                <SignedInLinks/>
             </div>
         </nav>
     )
