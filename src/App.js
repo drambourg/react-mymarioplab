c52080dce3ced34172c01cc2bd2bd7c841cfd16c
xNousex <damien.rambourg@gmail.com>
2019-08-16 15:36:21 +0200

    Implement React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
+import {BrowserRouter} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Mario Plan</h1>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <h1>Mario Plan</h1>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
