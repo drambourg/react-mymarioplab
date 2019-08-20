d1b11b696b2d125552c0cc4491ddc7e4cc0fd656
xNousex <damien.rambourg@gmail.com>
2019-08-20 11:57:37 +0200

    Implement async connect create project to Firebase via Redux

--- src/Store/Action/projectActions.js
+++ src/Store/Action/projectActions.js
@@ -1,9 +1,23 @@
-export const  createProject = (project) => {
-    return (dispatch, getState, { getFirebase, getFireStore }) => {
+export const createProject = (project) => {
+    return (dispatch, getState, {getFirebase, getFirestore}) => {
         //make async call to db
-        dispatch({
-            type : 'CREATE_PROJECT',
-            project
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authFirstName: 'Georges',
+            authLastName: 'Abitbol',
+            authId: 12345,
+            createAt: new Date(),
+        }).then(() => {
+            dispatch({
+                type: 'CREATE_PROJECT',
+                project
+            })
+        }).catch((err) => {
+            dispatch({
+                type: 'CREATE_PROJECT_ERROR',
+                err
+            })
         })
     }
 };
