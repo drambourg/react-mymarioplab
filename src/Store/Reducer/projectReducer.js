8e7431bb781014c026ed7319a0112da07747279a
xNousex <damien.rambourg@gmail.com>
2019-08-20 10:00:21 +0200

    Implements dispatch create project redux skeleton to async implemantation

--- src/Store/Reducer/projectReducer.js
+++ src/Store/Reducer/projectReducer.js
@@ -1,3 +1,5 @@
+import {createProject} from "../Action/projectActions";
+
 const initState = {
     projects: [
         {id: '1', title: 'help me find peach', content: 'blah blah blah'},
@@ -7,6 +9,10 @@ const initState = {
 }
 
 const projectReducer = (state = initState, action) => {
+    switch (action.type) {
+        case 'CREATE_PROJECT' :
+            console.log('createProject', action.project)
+    }
     return state
 }
 
